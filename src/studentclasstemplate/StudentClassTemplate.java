26d287c11f66201b4a27d85805702211ea97f6a1
patelsuj <patelsuj@sheridancollege.ca>
2019-05-16 16:47:18 -0400

    Student

---
+++ src/studentclasstemplate/StudentClassTemplate.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentclasstemplate;
+
+/**
+ *
+ * @author SUJAL
+ */
+public class StudentClassTemplate {
+
+    private String name;
+    
+    
+    public StudentClassTemplate(){}
+
+              
+public StudentClassTemplate(String n){
+
+      this.name = n;         
+}
+ 
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String setName(String name) {
+        this.name = name;
+        return name;
+         }
+}
+     
+       
+       
+    
---
+++ src/studentclasstemplate/StudentTester.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentclasstemplate;
+
+/**
+ *
+ * @author SUJAL
+ */
+public class StudentTester {
+    
+    public static void main(String args[]){
+    
+        
+        StudentClassTemplate s1 = new StudentClassTemplate("Sujal");
+        //s1.setName("Sujal");
+        StudentClassTemplate s2 = new StudentClassTemplate("Jeel");
+        //s2.setName("Jeel");
+        StudentClassTemplate s3 = new StudentClassTemplate("Diya");
+        //s3.setName("Jay");
+        
+        StudentClassTemplate[] list = new StudentClassTemplate[3];
+        list[0]= s1;
+        list[1]= s2;
+        list[2]= s3;
+        
+        for(int i = 0; i<list.length;i++){
+        System.out.println(list[i].getName());
+        }
+        
+    }
+}
